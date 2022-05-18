d2593885616cac1badcfb45ba559c49cfd62b580
Waliyah1 <105822588+Waliyah1@users.noreply.github.com>
2022-05-18 22:04:48 +0100

    Add files via upload
    Re-attempt of the first python assignment

---
+++ Assignment 1.py
@@ -0,0 +1,32 @@
+Name = input('Enter your name:')
+print ('Hello', Name)
+
+
+Hours = input ('Enter hours:')
+Rate = input ('Enter rate:')
+Pay = int(Hours) * int(Rate)
+print ('Pay:', Pay)
+
+
+Hours = int(input('Enter hours:'))
+Rate = int(input('Enter rate:'))
+Pay = Hours * Rate
+print ('Pay:', Pay)
+
+
+width = 17
+height = 12.0
+print(width//2)
+print(type(width))
+print(width/2.0)
+print(type(height))
+print(height/3)
+print(type(height))
+print(1+2*5)
+print(type(1+2*5))
+
+
+# Fahrenheit = (0°C x 9/5) + 35
+Celcius temperature = int(input('Enter Celcius temperature:'))
+Convert Fahrenheit = (Celcius temperature * 9/5) + 35
+print('Fahrenheit:', Convert Fahrenheit)
