a98ba85dd88bb3e4a83a853239855bc6b89c0848
Waliyah1 <105822588+Waliyah1@users.noreply.github.com>
2022-05-18 23:49:33 +0100

    Add files via upload
    Assignment on Chapter 3

---
+++ Assignment 2a.py
@@ -0,0 +1,22 @@
+#Exercise 1
+Hours = int(input('Enter hours:'))
+Rate = int(input('Enter rate:'))
+Pay = Hours * Rate
+if Hours > 40:
+	Pay = 1.5 * Hours * Rate
+	print(Pay)
+	
+	
+#Exercise 2
+Hours = int(input('Enter Hours:'))
+Rate = int(input('Enter Rate:'))
+try:
+	Hours = Hours
+	Rate = Name
+	print('Error, please enter numeric input')
+except:
+	Hours = forty
+	Rate = Rate
+	print('Error, please enter numeric input')
+	
+	
